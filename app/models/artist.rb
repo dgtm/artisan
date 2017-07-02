53bea5709fb1ce140ec625dfce6420e4cb484722
Dipesh Gautam <dipeshgtm@gmail.com>
2017-07-02 13:31:14 +0200

    Support searches by name and tags

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,20 @@
 class Artist < ApplicationRecord
   acts_as_taggable
 
-  def self.search(term)
+  scope :with_name_like, -> (term) {
     where("name LIKE ?", "%#{term}%")
+  }
+
+  scope :with_tags_like, -> (term) {
+    tagged_with(term, wild: true)
+  }
+
+  # @param [Hash] search_params
+  # @return [<ActiveRecord::Relation>] Collection of artists
+  def self.search(search_term)
+    return all unless search_term.present?
+
+    # Search both with name and tags
+    find_by_sql("((#{with_name_like(search_term).to_sql}) UNION (#{with_tags_like(search_term).to_sql}))")
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -9,15 +9,21 @@ RSpec.describe Artist, type: :model do
     end
 
     it "can search by name" do
-      expect(Artist.search("xyz")).to match_array([])
+      expect(Artist.search("xyz")).to be_empty
       expect(Artist.search("Andy")).to match_array([@andy])
       expect(Artist.search("Dave")).to match_array([@dave])
       expect(Artist.search("Matthews")).to match_array([@andy, @philip])
     end
 
     it "can search through tags" do
-      expect(Artist.search("dance").to_a).to eq([@dave])
-      expect(Artist.search("independent").to_a).to eq([@andy, @dave])
+      expect(Artist.search("dance")).to include(@dave)
+      expect(Artist.search("dance")).to_not include(@philip)
+      expect(Artist.search("independent")).to include(@andy, @dave)
+    end
+
+    it "returns back all records if no search term is provided" do
+      expect(Artist.search('')).to include(@andy, @dave, @philip)
+      expect(Artist.search(nil)).to include(@andy, @dave, @philip)
     end
   end
 end
